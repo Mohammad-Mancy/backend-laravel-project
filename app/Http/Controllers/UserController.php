79a747fa4007ed87839fb0705caf96a1cc0cb994
MohammadMancy <mohammad.mancy994@gmail.com>
2022-06-07 20:51:33 +0300

    Adding login API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -18,18 +19,24 @@ class UserController extends Controller
         $user->gender=$request->gender;
         $user->phone_number=$request->phone_number;
         $user->type='user';
-        // $user=array();
-        // $user['first_name']=$request->first_name;
-        // $user['last_name']=$request->last_name;
-        // $user['email']=$request->email;
-        // $user['password']=$request->password;
-        // $user['gender']=$request->gender;
-        // $user['phone_number']=$request->phone_number;
-        // $user['type']='user';
         $user->save();
         
         return response()->json([
             "status" => "Success"
         ], 200);
+    }
+    public function logIn(Request $request)
+    {
+        $user = User::where('email',$request->email)->first();
+        if(!$user){
+            return response()->json(['error' => 'user not found.'], 401);
+        }
+        if (!Hash::check($request->password,$user->password)) {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+        else {
+            return response()->json(['success' => 'success','id'=>$user->id,'name'=>$user->fname,'type'=>$user->type], 200);
+
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserController;
 
 
 Route::post('/register', [UserController::class, 'signUp']);
+Route::post('/login', [UserController::class, 'logIn']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
